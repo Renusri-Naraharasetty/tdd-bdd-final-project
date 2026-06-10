26f85f79a3157e1b9f84e73890dca7af5af2ce69
Renusri-Naraharasetty <naraharashettyrenusri@gmail.com>
2026-06-10 09:17:08 -0400

    Completed Exercise 3 REST API endpoints

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -28,10 +28,12 @@ import os
 import logging
 from decimal import Decimal
 from unittest import TestCase
+from urllib.parse import quote_plus
 from service import app
 from service.common import status
 from service.models import db, init_db, Product
 from tests.factories import ProductFactory
+
 
 # Disable all but critical errors during normal test run
 # uncomment for debugging failing tests
@@ -258,6 +260,80 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
 
         self.assertEqual(len(data), 5)
+    def test_query_by_name(self):
+        """It should Query Products by name"""
+        products = self._create_products(5)
+
+        test_name = products[0].name
+
+        name_count = len(
+            [product for product in products if product.name == test_name]
+        )
+
+        response = self.client.get(
+            BASE_URL,
+            query_string=f"name={quote_plus(test_name)}"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+
+        self.assertEqual(len(data), name_count)
+
+        for product in data:
+            self.assertEqual(product["name"], test_name)
+    def test_query_by_category(self):
+        """It should Query Products by category"""
+        products = self._create_products(10)
+
+        category = products[0].category
+
+        found = [
+            product for product in products
+            if product.category == category
+        ]
+
+        found_count = len(found)
+
+        response = self.client.get(
+            BASE_URL,
+            query_string=f"category={category.name}"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+
+        self.assertEqual(len(data), found_count)
+
+        for product in data:
+            self.assertEqual(product["category"], category.name)
+    def test_query_by_availability(self):
+        """It should Query Products by availability"""
+
+        products = self._create_products(10)
+
+        available_products = [
+            product for product in products
+            if product.available is True
+        ]
+
+        available_count = len(available_products)
+
+        response = self.client.get(
+            BASE_URL,
+            query_string="available=true"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+
+        self.assertEqual(len(data), available_count)
+
+        for product in data:
+            self.assertEqual(product["available"], True)
     #
 
     ######################################################################
