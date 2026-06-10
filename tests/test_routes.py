bcf168ed99e0577076034add67c34def752cc34e
Renusri-Naraharasetty <naraharashettyrenusri@gmail.com>
2026-06-10 08:59:09 -0400

    Completed List All Products test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -247,6 +247,17 @@ class TestProductRoutes(TestCase):
             new_count,
             product_count - 1
         )
+    def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+
+        response = self.client.get(BASE_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+
+        self.assertEqual(len(data), 5)
     #
 
     ######################################################################
